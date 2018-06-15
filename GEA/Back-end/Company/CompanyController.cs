6cdfcae514217cc9e34fc79721eda171c6430836
DaniloPSilva <danilo.dpspereira@gmail.com>
2018-06-15 19:07:34 -0300

    cria validador de cnpj

--- GEA/Back-end/Company/CompanyController.cs
+++ GEA/Back-end/Company/CompanyController.cs
@@ -58,6 +58,7 @@ namespace GEA.Back_end.Companies
         }
         #endregion
 
+        #region Método Responsável por atualizar Empresas
         [HttpPost]
         public JsonResult UpdateCompany(COMPANIES company)
         {
@@ -73,8 +74,9 @@ namespace GEA.Back_end.Companies
             else
               return Json(new { Success = false });
         }
+        #endregion
 
-
+        #region Método Responsável por Remover Empresas
         [HttpPost]
         public JsonResult DeleteCompany(int id)
         {
@@ -87,6 +89,7 @@ namespace GEA.Back_end.Companies
             }
             return Json(new { success = false });
         }
+        #endregion
 
         #region Método de validação do Salvar e Atualizar
         public static IList<ValidationFailures> validateSave(COMPANIES company)
@@ -116,10 +119,54 @@ namespace GEA.Back_end.Companies
                         Message = "Por favor informe o Cnpj da empresa."
                     });
                 }
+                var isValid = IsCnpj(company.Cnpj);
+
+                if (!isValid)
+                {
+                    failures.Add(new ValidationFailures
+                    {
+                        Message = "Por favor informe um Cnpj Válido."
+                    });
+                }
             }
             
             return failures;
         }
         #endregion
+
+        public static bool IsCnpj(string cnpj)
+        {
+            int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int soma;
+            int resto;
+            string digito;
+            string tempCnpj;
+            cnpj = cnpj.Trim();
+            cnpj = cnpj.Replace(".", "").Replace("-", "").Replace("/", "");
+            if (cnpj.Length != 14)
+                return false;
+            tempCnpj = cnpj.Substring(0, 12);
+            soma = 0;
+            for (int i = 0; i < 12; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCnpj = tempCnpj + digito;
+            soma = 0;
+            for (int i = 0; i < 13; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return cnpj.EndsWith(digito);
+        }
     }
 }
