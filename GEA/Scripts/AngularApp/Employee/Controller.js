96b575b2cbd9ebbabcf5f73b8bfc6bca0d5d9d54
DaniloPSilva <danilo.dpspereira@gmail.com>
2018-06-17 14:20:43 -0300

    cria controlador e service js do cadastro de funcionario

---
+++ GEA/Scripts/AngularApp/Employee/Controller.js
@@ -0,0 +1,93 @@
+﻿/*
+ Author: DaniloPereira
+Cria controllador, onde vai estar todas as funções 
+ 
+ */
+
+//lista todas as empresas cadastradas
+employeeApp.controller('EmployeeController', function ($scope, employeeService) {
+
+    getEmployees();
+
+    function getEmployees() {
+        $scope.Employees = null;
+        employeeService.getEmployees().then(result => {
+            $scope.Employees = result.data;
+            }, function () {
+                alert("Um erro ocorreu");
+            })
+    }
+
+    // cria objeto funcionário para salvar.
+    $scope.saveEmployee = function () {
+        let saveEmployeeInput = {
+            id: $scope.id,
+            name: $scope.name,
+            cpf: $scope.cpf
+        };
+
+        let employee = employeeService.saveEmployee(saveEmployeeInput);
+
+            //valida se funcionario foi cadastrado e retorma mensagens ao usuário.
+        employee.then(result => {
+                if (result.data.success === true) {
+                    getEmployees();
+                    $scope.clearFields();
+                } else if (result.data)
+                    alert(result.data.failures);
+            });
+    }
+
+    $scope.updateEmployeeId = function(employee) {
+        $scope.employeeId = employee.Id;
+        $scope.employeeName = employee.Name;
+        $scope.employeeCpf = employee.Cpf;
+    }
+
+    $scope.updateEmployee = function() {
+        let employeeInput = {
+            Id: $scope.employeeId,
+            Name: $scope.employeeName,
+            Cnpj: $scope.employeeCnpj,
+        };
+
+        let employeeData = employeeService.updateEmployee(employeeInput);
+        employeeData.then(result => {
+            if (result.data.success === true) {
+                getEmployees();
+                $scope.clearFields();
+            }
+            else {
+                alert(result.data.failures);
+            }
+        }, function () {
+            alert("Ocorreu um erro ao tentar atualizar o funcionário!");
+        });
+    }
+
+    $scope.deleteEmployee = function(id){
+        var deleteData = employeeService.deleteEmployee(id);
+            deleteData.then(result => {
+                if(result.data.success ===true)
+                    getEmployees();
+                else{
+                    alert("Funcionário não removido ");
+                }
+            }, function(){
+                alert("Ocorreu ao tentar Remover o Funcionário");
+            });
+    }
+
+    $scope.deleteEmployeeId = function(employee) {
+        $scope.employeeId = employee.Id;
+        $scope.employeeName = employee.Name;
+    }
+
+
+    // Funcão responsável para limpar os campos.
+    $scope.clearFields = function () {
+        $scope.id = null;
+        $scope.name = null;
+        $scope.cnpj = null;
+    }
+});
---
+++ GEA/Scripts/AngularApp/Employee/Service.js
@@ -0,0 +1,36 @@
+﻿/*
+ Author: DaniloPereira
+ 
+ */
+
+// Service responsável por listar todas os funcionários
+employeeApp.service('employeeService', function ($http) {
+    this.getEmployees = function () {
+        return $http.get('/Employee/GetEmployees');
+    }
+
+    //Service responsavel por salvar novo funcionário
+    this.saveEmployee = function(employee){
+        var request = $http({
+            method: 'post',
+            url: '/Employee/SaveEmployee',
+            data: employee
+        });
+        return request; 
+    }
+
+    //Service responsavel por editar um funcionário
+    this.updateEmployee = function (employee) {
+        var request = $http({
+            method: 'post',
+            url: '/Employee/UpdateEmployee',
+            data: employee
+        });
+        return request;
+    }
+
+    //Service responsavel por editar uma empresa
+    this.deleteEmployee = function (id) {
+        return $http.post('/Employee/DeleteEmployee/'+ id);
+    }
+});
